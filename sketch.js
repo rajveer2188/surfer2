04fc3f5ec0b5ac262684245b19656ef4bd3d26eb
rajveer2188 <92036661+rajveer2188@users.noreply.github.com>
2021-12-02 19:13:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -72,8 +72,8 @@ function draw() {
 
       // treasureCollection=+ 150;
       // treasureCollection= 150;
-      treasureCollection= treasureCollection - 150;
-      // treasureCollection= treasureCollection + 150;
+      // treasureCollection= treasureCollection - 150;
+      treasureCollection= treasureCollection + 150;
       
     }else{
       if(swordGroup.isTouching(boy)) {
@@ -98,10 +98,10 @@ function draw() {
         // jewelryG.destroy();
         // swordGroup.destroy();
         
-        // cashG.destroyEach();
-        // diamondsG.destroyEach();
-        // jewelryG.destroyEach();
-        // swordGroup.destroyEach();
+        cashG.destroyEach();
+        diamondsG.destroyEach();
+        jewelryG.destroyEach();
+        swordGroup.destroyEach();
         
         // cashGdestroyEach();
         // diamondsGdestroyEach();
